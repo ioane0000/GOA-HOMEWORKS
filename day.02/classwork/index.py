72bd3245a71c372b549ccc7b79579caba8fc55a2
ioane0000 <ioanejishkariani12@gmail.com>
2025-02-07 18:45:49 +0400

    seccond commit

--- day.02/classwork/index.py
+++ day.02/classwork/index.py
@@ -1,1 +1,17 @@
-# we dint have classwork
+num1 = "ioane"
+num2 = "jishkariani"
+num3 = 14
+
+print(num1)
+print(num2)
+print(num3)
+
+#  2
+
+name = "ioane"
+surname = "jishkariani"
+age = 14
+hobby = "football"
+car = "Ferrari sf stradall"
+
+print(f"{name} {surname} {age} {hobby} {car}")
