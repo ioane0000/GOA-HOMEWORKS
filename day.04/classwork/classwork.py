7443a7d4fb0929c851b48b1b1a99159607f5e54e
ioane0000 <ioanejishkariani12@gmail.com>
2025-02-07 20:38:09 +0400

    seccond commit

---
+++ day.04/classwork/classwork.py
@@ -0,0 +1,410 @@
+string1 = "Hello"
+string2 = "World"
+string3 = "Python"
+string4 = "Programming"
+
+int1 = 5
+int2 = 10
+int3 = 15
+int4 = 20
+
+float1 = 3.14
+float2 = 2.71
+float3 = 1.618
+float4 = 9.8
+
+print(f"String values: {string1}, {string2}, {string3}, {string4}")
+print(f"Integer values: {int1}, {int2}, {int3}, {int4}")
+print(f"Float values: {float1}, {float2}, {float3}, {float4}")
+
+
+#2
+
+# +
+
+a = 2
+b = 4
+print(a + b)
+
+a = 75
+b = 34
+print(a + b)
+
+a = 73
+b = 21
+print(a + b)
+
+a = 21
+b = 12
+print(a + b)
+
+a = 76
+b = 46
+print(a + b)
+
+a = 654
+b = 58
+print(a + b)
+
+a = 13
+b = 86
+print(a + b)
+
+a = 342
+b = 128
+print(a + b)
+
+a = 81
+b = 61
+print(a + b)
+
+a = 35
+b = 27
+print(a + b)
+
+a = 1
+b = 4
+print(a + b)
+
+a = 3
+b = 63
+print(a + b)
+
+a = 23
+b = 53
+print(a + b)
+
+a = 64
+b = 34
+print(a + b)
+
+a = 43
+b = 54
+print(a + b)
+
+# -
+
+a = 32
+b = 52
+print(a - b)
+
+a = 2
+b = 52
+print(a - b)
+
+a = 65
+b = 44
+print(a - b)
+
+a = 34
+b = 2
+print(a - b)
+
+a = 63
+b = 34
+print(a - b)
+
+a = 634
+b = 34
+print(a - b)
+
+a = 53
+b = 23
+print(a - b)
+
+a = 658
+b = 23
+print(a - b)
+
+a = 23
+b = 43
+print(a - b)
+
+a = 5
+b = 23
+print(a - b)
+
+a = 56
+b = 86
+print(a - b)
+
+a = 76
+b = 45
+print(a - b)
+
+a = 23
+b = 322
+print(a - b)
+
+a = 2
+b = 23
+print(a - b)
+
+# *
+
+a = 12
+b = 24
+print(a * b)
+
+a = 21
+b = 42
+print(a * b)
+
+a = 3
+b = 45
+print(a * b)
+
+a = 4
+b = 345
+print(a * b)
+
+a = 5
+b = 75
+print(a * b)
+
+a = 6
+b = 23
+print(a * b)
+
+a = 743
+b = 23
+print(a * b)
+
+a = 8
+b = 43
+print(a * b)
+
+a = 9 
+b = 3982
+print(a * b)
+
+a = 10
+b = 671
+print(a * b)
+
+a = 111
+b = 32
+print(a * b)
+
+a = 1233
+b = 322
+print(a * b) 
+
+a = 12335
+b = 212
+print(a * b)
+
+a = 142
+b = 2
+print(a * b)
+
+a = 1512
+b = 24
+print(a * b)
+
+# /
+
+a = 5
+b = 1
+print(a / b)
+
+a = 2
+b = 32
+print(a / b)
+
+a = 34
+b = 2
+print(a / b)
+
+a = 4
+b = 2
+print(a / b)
+
+a = 5
+b = 2
+print(a / b)
+
+a = 6
+b = 2
+print(a / b)
+
+a = 7
+b = 2
+print(a / b)
+
+a = 8
+b = 2
+print(a / b)
+
+a = 9
+b = 2
+print(a / b)
+
+a = 10
+b = 2
+print(a / b)
+
+a = 11
+b = 2
+print(a / b)
+
+a = 12
+b = 2
+print(a / b)
+
+a = 13
+b = 2
+print(a / b)
+
+a = 14
+b = 2
+print(a / b)
+
+a = 15
+b = 2
+print(a / b)
+
+# %
+
+a = 2
+b = 3
+print(a % b)
+
+a = 32
+b = 3
+print(a % b)
+
+a = 3
+b = 2
+print(a % b)
+
+a = 34
+b = 3
+print(a % b)
+
+a = 565
+b = 223
+print(a % b)
+
+a = 8
+b = 7
+print(a % b)
+
+a = 9
+b = 5
+print(a % b)
+
+a = 8
+b = 23
+print(a % b)
+
+a = 9
+b = 12
+print(a % b)
+
+a = 10
+b = 26
+print(a % b)
+
+a = 11
+b = 3
+print(a % b)
+
+a = 12
+b = 4
+print(a % b)
+
+a = 13
+b = 54
+print(a % b)
+
+
+a = 14
+b = 12
+print(a % b)
+
+a = 13
+b = 5
+print(a % b)
+
+# **
+
+a = 1
+b = 2
+print(a ** b)
+
+a = 2
+b = 2
+print(a ** b)
+
+a = 3
+b = 2
+print(a ** b)
+
+
+a = 4
+b = 2
+print(a ** b)
+
+a = 5
+b = 2
+print(a ** b)
+
+a = 6
+b = 2
+print(a ** b)
+
+a = 7
+b = 2
+print(a ** b)
+
+a = 8
+b = 2
+print(a ** b)
+
+a = 9
+b = 2
+print(a ** b)
+
+a = 10
+b = 2
+print(a ** b)
+
+a = 11
+b = 21
+print(a ** b)
+
+a = 12
+b = 2
+print(a ** b)
+
+a = 13
+b = 2
+print(a ** b)
+
+a = 14
+b = 2
+print(a ** b)
+
+a = 15
+b = 2
+print(a ** b)
+
+# 3
+
+a = 1213
+
+b = 21
+
+c = 32232
+
+d = 1212465
+
+e = 34
+
+f = 43
+
+g = 23
+
+
+print(a + d - c * b % e / f ** g)
