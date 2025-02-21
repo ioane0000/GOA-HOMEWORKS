1fab58590c45155c7329b9080cc67be2da901d62
ioane0000 <ioanejishkariani12@gmail.com>
2025-02-21 21:31:04 +0400

    seccond commit

---
+++ day.19/classwork.py
@@ -0,0 +1,21 @@
+for i in range(150, 203, 2):
+    print(i)
+
+# 2
+
+for i in range(400, 199, -1):
+    print(i)
+
+# 3
+
+i = 0
+while i < 50:
+    print("GOA BEST!!!")
+    i += 1
+
+# 4
+
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
---
+++ day.19/homework.py
@@ -0,0 +1,38 @@
+# 1
+i = 0
+while i < 100:
+    print("GOA BEST!!!")
+    i += 1
+
+# 2
+
+for i in range(100):
+    print("GOA BEST!!!")
+
+# 3
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+# 4
+
+i = 1
+sum = 0
+while i <= 50:
+    sum += i
+    i += 1
+
+# 5
+
+names = ["ნიკა", "თეო", "ანა", "გიო"]
+
+count = 0
+for name in names:
+    count += 1
+
+# 6 
+
+for i in range(7):
+    print("მე მიყვარს პროგრამირება")
