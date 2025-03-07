7f2b4cc2cf954a55a3a77d27104412cd08e151e7
ioane0000 <ioanejishkariani12@gmail.com>
2025-03-07 13:41:25 +0400

    seccond commit

---
+++ day.21/classwork/classwork.py
@@ -0,0 +1,21 @@
+# 1
+favorite_cars = ["Ferrari SF stradale", "BMW M3", "Audi R8", "Porsche 911", "Ferrari 488"]
+
+print(favorite_cars)
+#2 
+
+favorite_foods=["pizza,khinkali,BBC"]
+
+print(favorite_foods)
+#3
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+print("Whole list:", num)
+
+
+print("From 2 to 8:", num[1:8])
+
+
+print("From 5 to 10:", num[4:10])
